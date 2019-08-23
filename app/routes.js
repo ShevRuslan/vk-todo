fc743847d0199788fa0a556e8bb6fdbdaf2c632f
sasa02sasa <sasa02sasa@mail.ru>
2019-08-23 20:42:52 +0600

    Удален vue-router

--- app/routes.js
+++
@@ -1,7 +0,0 @@
-import HelloWorld from './components/HelloWorld.vue'
-import RouterTest from './components/RouterTest.vue'
-
-export default [
-    { path: '/:page/hello-world', component: HelloWorld, name: 'HelloWorld' },
-    { path: '/:page/router-test', component: RouterTest, name: 'RouterTest' }
-]
