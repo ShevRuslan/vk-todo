f15c530c9375d4736e3dce3f60e41bfa8df180bb
sasa02sasa <sasa02sasa@mail.ru>
2019-08-28 22:42:33 +0600

    Создание базовой верстки для создание задач

--- app/components/TodoCreateElement.vue
+++ app/components/TodoCreateElement.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        
+        <input type="text"  class="text ts_input" id="ts_input" autocomplete="off" name="disable-autofill" >
+        <div class="line_cell clear_fix ui_rmenu_item_sel">
+            <div class="option_name">Создать задачу!</div>
+        </div>
     </div>
 </template>
 <script>
